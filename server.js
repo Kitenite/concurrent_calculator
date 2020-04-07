e64f68eca80b4c880e627a0529f78f208db7d7f9
Kiet Ho <hoakiet98@gmail.com>
2020-04-07 18:35:23 -0500

    aesthetics

--- server.js
+++ server.js
@@ -7,51 +7,49 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 PORT = 3000;
 
-// Methods
+// Get Index
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
+// Get style sheets
+app.get('/styles.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/styles.css'));
+});
+// Get data
 app.get('/calculations.json', function(req, res) {
     res.sendFile(path.join(__dirname + '/calculations.json'));
 });
-
+// Post data
 app.post('/postCalculations', function(req, res) {
     addCalc(req.body);
     res.redirect(req.get('referer'));
 });
-
-
-
-// io.sockets.on('connection', function(socket) {
-//   console.log("User connected");
-//   fs.watchFile('./calculations.json', {persistent:true}, function(data) {
-//       socket.emit('server', {message: 'File changed'});
-//   });
-// });
-
+// Start server
 var server = app.listen(PORT, function () {
    var host = server.address().address;
    var port = server.address().port;
    console.log(`Listening at http://localhost:${PORT}`);
 });
 
-// Socket.io
+// Setup socketio for concurrency
 var io = require('socket.io').listen(server);
-// Functions
+
+// Process and add calculation data to JSON file
 function addCalc(body){
   var fs = require('fs');
-    var file_path = 'calculations.json';
-    fs.readFile(file_path, function (err, data) {
-      expression = body.calculation +"=" + eval(body.calculation);
-      console.log(expression)
-      var entry = {
-        timeStamp: Date.now(),
-        calculation: expression
-      }
-      var json = JSON.parse(data);
-      json.entries.unshift(entry);
-      fs.writeFileSync(file_path, JSON.stringify(json));
-    });
+  var file_path = 'calculations.json';
+  fs.readFile(file_path, function (err, data) {
+    // Evaluate expression
+    expression = body.calculation +"=" + eval(body.calculation);
+    var entry = {
+      timeStamp: Date.now(),
+      calculation: expression
+    }
+    // Add to "database"
+    var json = JSON.parse(data);
+    json.entries.unshift(entry);
+    fs.writeFileSync(file_path, JSON.stringify(json));
+  });
+  // Broadcast change
   io.emit('update');
 }
