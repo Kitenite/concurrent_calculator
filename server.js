bed57cd52d310654b5bb817290f73a0c46056027
Kiet Ho <hoakiet98@gmail.com>
2020-04-07 17:36:08 -0500

    done, need some aesthetics

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var http = require('http');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-port = 3000;
-const date = new Date();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+PORT = 3000;
 
 // Methods
 app.get('/', function(req, res) {
@@ -21,16 +21,25 @@ app.post('/postCalculations', function(req, res) {
     res.redirect(req.get('referer'));
 });
 
-var server = app.listen(port, function () {
+
+
+// io.sockets.on('connection', function(socket) {
+//   console.log("User connected");
+//   fs.watchFile('./calculations.json', {persistent:true}, function(data) {
+//       socket.emit('server', {message: 'File changed'});
+//   });
+// });
+
+var server = app.listen(PORT, function () {
    var host = server.address().address;
    var port = server.address().port;
-   console.log(`Listening at http://localhost:${port}`);
+   console.log(`Listening at http://localhost:${PORT}`);
 });
 
+// Socket.io
+var io = require('socket.io').listen(server);
 // Functions
-
 function addCalc(body){
-  console.log(body);
   var fs = require('fs');
     var file_path = 'calculations.json';
     fs.readFile(file_path, function (err, data) {
@@ -42,7 +51,7 @@ function addCalc(body){
       }
       var json = JSON.parse(data);
       json.entries.unshift(entry);
-      console.log(entry);
       fs.writeFileSync(file_path, JSON.stringify(json));
     });
+  io.emit('update');
 }
